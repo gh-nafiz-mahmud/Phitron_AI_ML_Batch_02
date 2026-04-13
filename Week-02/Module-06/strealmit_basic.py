a857265fd8c43f1c45186767d8ec3878c63e88dc
Nafiz Mahmud <work.nafiz@gmail.com>
2026-04-13 11:31:09 +0600

    learning streamlit framework

--- Week-02/Module-06/strealmit_basic.py
+++ Week-02/Module-06/strealmit_basic.py
@@ -1,1 +1,36 @@
-print("Hello world")
+import streamlit as st
+
+# st.write("Hello world")
+# st.title(":red[My first streamlit web applicatin]", anchor=False, width="content")
+# st.header("Content 1", divider=True, anchor=False)
+# st.subheader("Content 1: sub header")
+# st.text("Hello world")
+# st.markdown(":red-background[*Hello world*]")
+# st.markdown(":green[:red[*Hello] world*]")
+# st.markdown(":thought_balloon: :blue[*thinking*]")
+
+# a = 10
+# b = 20
+# st.write(f"a + b == {a + b}")
+
+# Input Element in streamlit
+
+# test, number, choose option
+
+name = st.text_input("Enter text: ")
+print(type(name))
+# st.write(f"your name is {name}")
+
+age = st.number_input("Enter age: ", value=None, placeholder="type your age")
+print(type(age))
+# st.write(f"Number is: {age}")
+
+password = st.text_input("Enter password: ", type="password")
+print(type(password))
+# st.write(f"your password is {password}")
+
+# button
+
+pressed = st.button("Enter To Confirmed", type="primary")
+if pressed:
+    st.write(f"Your name is {name}, age {age}, password {password}")
