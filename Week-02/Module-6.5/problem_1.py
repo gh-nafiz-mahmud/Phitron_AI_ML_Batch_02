c214dd016fe4a2d34ab5e524a97c5cbbab2cbf31
gh-nafiz-mahmud <work.nafiz@gmail.com>
2026-04-29 21:59:41 +0600

    solving problems

---
+++ Week-02/Module-6.5/problem_1.py
@@ -0,0 +1,21 @@
+import streamlit as st
+
+n1 = st.number_input("Enter first number: ", value=None)
+n2 = st.number_input("Enter second number: ", value=None)
+
+operation = st.selectbox(
+    "Choose operation",
+    ('+', '-', '*', '/'),
+    index= None
+)
+
+butt = st.button("Calculate")
+if butt:
+    if operation == '+':
+        st.write(f'sum is {n1 + n2}')
+    elif operation == '-':
+        st.write(f'sub is {n1 - n2}')
+    elif operation == '*':
+        st.write(f'mul is {n1 * n2}')
+    elif operation == '/':
+        st.write(f'div is {n1 / n2}')
---
+++ Week-02/Module-6.5/problem_2.py
@@ -0,0 +1,17 @@
+import streamlit as st
+from google import genai
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+get_api = os.environ.get("API_KEY")
+client = genai.Client(api_key=get_api)
+
+user_content = st.text_input("Enter your prompt: ")
+response = client.models.generate_content(
+    model="gemini-3-flash-preview",
+    contents = f"Explain this question in 100 words {user_content}"
+)
+st.divider()
+
+st.markdown(response.text)
